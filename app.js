17339798270044ac7b7b234d772384dd243b353d
ruzica-glavas <ruzi99@hotmail.it>
2025-03-18 12:01:03 +0100

    add: install CORS

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ import express from "express";
 
 //Impostazione di express e la porta del server
 const app = express();
+import cors from 'cors';
 const port = process.env.SERVER_PORT || 3000;
 
 import movieRouter from "./routes/movieRouter.js"
+
+//middleware cors
+app.use(
+  cors({
+    origin: process.env.FRONTEND_APP,
+  })
+)
 
 //Router libri
 app.use("/movies", movieRouter);
